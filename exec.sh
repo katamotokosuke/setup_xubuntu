5966d7f233b438462c45fe643b98ca804992ff75
KosukeKatamoto <katamotokosuke0605@gmail.com>
2018-03-06 08:34:25 +0900

    first commit

---
+++ exec.sh
@@ -0,0 +1,30 @@
+# 日本語入力
+sudo apt-get install fcitx-mozc
+# git
+sudo apt-get install git
+# rbenv install
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+# emacs install
+sudo apt-get install emacs24
+
+#virtualbox latest versionをinstall
+#----------------------------------
+# deb https://download.virtualbox.org/virtualbox/debian <mydist> contrib
+# を/etc/apt/sources.listの末尾に付け加える
+# wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+# wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+#sudo apt-get update
+#sudo apt-get install virtualbox-5.2
+#---------------------------------------
+
+
+# vagrant latest versionをinstall
+# -------------------------------
+# wget https://releases.hashicorp.com/vagrant/2.0.2/vagrant_2.0.2_x86_64.deb
+# sudo dpkg -i vagrant_2.0.2_x86_64.deb
+# vagrant version
+# ------------------------------
+
+# git client をinstall
+sudo apt-get install tig
+
